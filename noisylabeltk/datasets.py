b084a9a08170af9074f2ea38c83ea8b3732927f0
Ygor Canalli <ygor.canalli@gmail.com>
2020-12-22 19:03:01 -0300

    adding info to datasets

--- noisylabeltk/datasets.py
+++ noisylabeltk/datasets.py
@@ -5,36 +5,69 @@ import tensorflow_datasets as tfds
 
 #%%
 def load_mnist():
-    train_ds, test_ds = tfds.load('mnist', split=['train', 'test'])
-    return train_ds, test_ds
+    (train_ds, test_ds), ds_info = tfds.load('mnist',
+                                    split=['train', 'test'],
+                                    as_supervised=True,
+                                    with_info=True)
+    return (train_ds, test_ds), ds_info
 
 def load_fashionmnist():
-    train_ds, test_ds = tfds.load('fashion_mnist', split=['train', 'test'])
-    return train_ds, test_ds
+    (train_ds, test_ds), ds_info = tfds.load('fashion_mnist',
+                                    split=['train', 'test'],
+                                    as_supervised=True,
+                                    with_info=True)
+    return (train_ds, test_ds), ds_info
 
 def load_cifar10():
-    train_ds, test_ds = tfds.load('cifar10', split=['train', 'test'])
-    return train_ds, test_ds
+    (train_ds, test_ds), ds_info = tfds.load('cifar10', 
+                                    split=['train', 'test'],
+                                    shuffle_files=True,
+                                    as_supervised=True,
+                                    with_info=True)
+    
+
+    return (train_ds, test_ds), ds_info
 
 def load_cifar100():
-    train_ds, test_ds = tfds.load('cifar100', split=['train', 'test'])
-    return train_ds, test_ds
+    (train_ds, test_ds), ds_info = tfds.load('cifar100', 
+                                    split=['train', 'test'],
+                                    shuffle_files=True,
+                                    as_supervised=True,
+                                    with_info=True)
+    
+
+    return (train_ds, test_ds), ds_info
 
 def load_svhn():
-    train_ds, test_ds = tfds.load('svhn_cropped', split=['train', 'test'])
-    return train_ds, test_ds
+    (train_ds, test_ds), ds_info = tfds.load('svhn_cropped',
+                                    split=['train', 'test'],
+                                    shuffle_files=True,
+                                    as_supervised=True,
+                                    with_info=True)
+    return (train_ds, test_ds), ds_info
 
 def load_imagenet():
-    train_ds, test_ds = tfds.load('imagenet2012', split=['train', 'test'])
-    return train_ds, test_ds
+    (train_ds, test_ds), ds_info = tfds.load('imagenet2012',
+                                    split=['train', 'test'],
+                                    shuffle_files=True,
+                                    as_supervised=True,
+                                    with_info=True)
+    return (train_ds, test_ds), ds_info
 
 def load_openimage():
-    train_ds, test_ds = tfds.load('open_images_challenge2019_detection', split=['train', 'test'])
-    return train_ds, test_ds
+    (train_ds, test_ds), ds_info = tfds.load('open_images_challenge2019_detection',
+                                    split=['train', 'test'],
+                                    shuffle_files=True,
+                                    as_supervised=True,
+                                    with_info=True)
+    return (train_ds, test_ds), ds_info
 
 def load_germancredit():
-    train_ds, test_ds = tfds.load('german_credit_numeric', split=['train[:80%]', 'train[-20%:]'])
-    return train_ds, test_ds
+    (train_ds, test_ds), ds_info = tfds.load('german_credit_numeric',
+                                    split=['train[:80%]', 'train[-20%:]'],
+                                    as_supervised=True,
+                                    with_info=True)
+    return (train_ds, test_ds), ds_info
 
     
 
